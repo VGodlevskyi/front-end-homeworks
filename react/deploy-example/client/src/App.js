ef79388f4c56afa2da38f51ea84e3447e48724af
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-01-05 13:29:20 +0200

    material-done_deploy-bigin

---
+++ react/deploy-example/client/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import {Accordion} from "@material-ui/core";
+import CustomizedAccordions from "./components/Accordion";
+import PrimarySearchAppBar from "./components/AppBar";
+
+function App() {
+    return (
+        <div className="App">
+            <PrimarySearchAppBar/>
+            <CustomizedAccordions/>
+
+            <Container className={classes.cardGrid} maxWidth="md">
+                {/* End hero unit */}
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        View
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Edit
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
