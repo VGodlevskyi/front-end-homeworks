54486c001b32997fe0fccd93ed585669a70271dc
Vitalii Godlevskyi <vgodlevskyi@gmail.com>
2021-01-05 14:13:27 +0200

    add project

--- react/deploy-example/client/src/App.js
+++ react/deploy-example/client/src/App.js
@@ -3,8 +3,57 @@ import './App.css';
 import {Accordion} from "@material-ui/core";
 import CustomizedAccordions from "./components/Accordion";
 import PrimarySearchAppBar from "./components/AppBar";
+import {useEffect, useState} from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
 
 function App() {
+    const [cards, setCards] = useState([])
+    const classes = useStyles();
+    useEffect(() => {
+        fetch('api/emails')
+            .then(r => r.json())
+            .then(data => {
+                setCards(data.inbox || [])
+            })
+
+    }, [])
     return (
         <div className="App">
             <PrimarySearchAppBar/>
@@ -14,29 +63,16 @@ function App() {
                 {/* End hero unit */}
                 <Grid container spacing={4}>
                     {cards.map((card) => (
-                        <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Grid item key={card.id} xs={12} sm={6} md={4}>
                             <Card className={classes.card}>
-                                <CardMedia
-                                    className={classes.cardMedia}
-                                    image="https://source.unsplash.com/random"
-                                    title="Image title"
-                                />
                                 <CardContent className={classes.cardContent}>
                                     <Typography gutterBottom variant="h5" component="h2">
-                                        Heading
+                                        {card.subject}
                                     </Typography>
                                     <Typography>
-                                        This is a media card. You can use this section to describe the content.
+                                        {card.text}
                                     </Typography>
                                 </CardContent>
-                                <CardActions>
-                                    <Button size="small" color="primary">
-                                        View
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Edit
-                                    </Button>
-                                </CardActions>
                             </Card>
                         </Grid>
                     ))}
